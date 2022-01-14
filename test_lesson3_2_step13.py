7482e426402a03fbd3fc085d824be4458d9902d5
NickerLog <Lobov2000@gmail.com>
2022-01-14 19:16:07 +0300

    added implicitly_wait, deleted time.sleep

--- test_lesson3_2_step13.py
+++ test_lesson3_2_step13.py
@@ -6,6 +6,7 @@ class RegistrationTest(unittest.TestCase):
     def test_registration1(self):
         link = "http://suninjuly.github.io/registration1.html"
         browser = webdriver.Chrome()
+        browser.implicitly_wait(5)
         browser.get(link)
         input1 = browser.find_element_by_css_selector(".form-control.first[required]")
         input1.send_keys("12")
@@ -15,15 +16,14 @@ class RegistrationTest(unittest.TestCase):
         input1.send_keys("1234")
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
-        time.sleep(1)
         welcome_text_elt = browser.find_element_by_tag_name("h1")
         welcome_text = welcome_text_elt.text
-        time.sleep(10)
         browser.quit()
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Failed Registration #1")
     def test_registration2(self):
         link = "http://suninjuly.github.io/registration2.html"
         browser = webdriver.Chrome()
+        browser.implicitly_wait(5)
         browser.get(link)
         input1 = browser.find_element_by_css_selector(".form-control.first[required]")
         input1.send_keys("12")
@@ -33,10 +33,8 @@ class RegistrationTest(unittest.TestCase):
         input1.send_keys("1234")
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
-        time.sleep(1)
         welcome_text_elt = browser.find_element_by_tag_name("h1")
         welcome_text = welcome_text_elt.text
-        time.sleep(10)
         browser.quit()
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Failed Registration #2")
 
